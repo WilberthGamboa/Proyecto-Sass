c23274971a3c6ea41e00b416bf397e71da9559b1
wilberth <wgcampos15@gmail.com>
2022-08-08 15:08:37 -0500

    notas llamada tareas

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+
+const {src, dest} = require("gulp"); //EXTRAE LA FUNCIONAL DEL JSON GULP
+//src permite identificar un archivo 
+//dest sirve para guardarlo 
+
+function css(done){
+    //Indentificar el archivo SASS
+
+
+    done(); //Call back que avisa a gulp cuando lleguamos al final
+}
---
+++ notas.md
@@ -0,0 +1,8 @@
+# LLamar tareas 
+
+## Llamar tareas por medio de npx
+Para llamar las tareas creadas en gulpfile.js se utiliza el comando npx gulp [nombre de la tarea]
+
+## Llamar tareas por medio del package.json por npm 
+
+
