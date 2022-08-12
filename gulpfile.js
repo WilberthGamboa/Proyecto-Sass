aa321255cf209e9af3d3ded1cb50aaf3067ee40c
wilberth <wgcampos15@gmail.com>
2022-08-11 22:09:23 -0500

    agregando normalize

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 //COMPILAR ARCHIVOS SCSS CON GULP 
 const {src, dest,watch} = require("gulp"); //EXTRAE LA FUNCIONAL DEL JSON GULP
 const sass = require("gulp-sass")(require('sass'));
+const plumber = require('gulp-plumber');
 //src permite identificar un archivo 
 //dest sirve para guardarlo 
 // igual está la funcion de watch
@@ -9,7 +10,10 @@ function css(done){
     //Indentificar el archivo SASS
     //Aquí utilizaremos pipe, identifica una acción que se realiza después de otra
     //src('src/scss/app.scss').pipe(sass()).pipe(dest("build/css"))  , cambiaremos la sintaxis para que escuche todas
-    src('src/scss/**/*.scss').pipe(sass()).pipe(dest("build/css"))
+    src('src/scss/**/*.scss')
+    .pipe(plumber()) //No detiene la ejecucion incluso si hay errores
+    .pipe(sass())
+    .pipe(dest("build/css"))
 
     done(); //Call back que avisa a gulp cuando lleguamos al final
 }
